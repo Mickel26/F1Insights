6cc87cf29225b51d9e779bd02b86bbc6015643a0
Mickel26 <mikolajfilka@gmail.com>
2025-09-18 21:38:46 +0200

    Fixed key issue

--- src/components/SeasonList.tsx
+++ src/components/SeasonList.tsx
@@ -9,10 +9,15 @@ export default async function SeasonsList() {
     <div className="p-4">
       <h1 className="text-xl font-bold mb-2">Available seasons:</h1>
       <ul className="list-disc ml-6">
-        {years.map((year) => (
-          <Link href={`/seasons/${year}`} className="text-blue-500 hover:underline">
-            <li key={year}>{year}</li>
-          </Link>
+        {years.map((year, key) => (
+          <li key={year}>
+            <Link
+              href={`/seasons/${year}`}
+              className="text-blue-500 hover:underline"
+            >
+              {year}
+            </Link>
+          </li>
         ))}
       </ul>
     </div>
